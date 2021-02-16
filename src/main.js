08149c66cdd23a575d510ddefe29813a1c20bc25
alifma <alifmaulana26@gmail.com>
2021-02-16 21:23:42 +0700

    Base Login Screen

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+// Bootstrap & Bootstrap-Vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '../src/assets/css/style.css'
 
+Vue.config.productionTip = false
+// Use Bootstrap & Bootstrap-Vue
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container" style="height:100vh">
+    <div class="row h-100">
+      <div class="col-sm-12 col-md-2 col-lg-3 my-auto">
+      </div>
+      <div class="col-sm-12 col-md-8 col-lg-6 my-auto">
+        <div class="card p-4 shadow" style="border-radius:25px">
+          <div class="card-body">
+            <h4 class="text-main text-center my-3">Login</h4>
+            <p class="mt-4">Hi, Welcome Back!</p>
+            <form action="" class="my-4">
+              <div class="form-group mb-4">
+                <label class="text-secondary" >Email</label>
+                <input type="email" class="px-0 font-weight-bold form-control border-top-0 border-left-0 border-right-0" style="border-bottom:2px solid black" value="obroline@gmail.com">
+              </div>
+              <div class="form-group mt-4">
+                <label class="text-secondary" >Password</label>
+                <input type="password" value="password" class="px-0 font-weight-bold form-control border-top-0 border-left-0 border-right-0" style="border-bottom:2px solid black">
+              </div>
+              <div class="form-group mt-4 text-right">
+                <label class="text-main" >Forgot passsword?</label>
+              </div>
+              <button class="btn btn-main w-100 font-weight-bold my-3" style="border-radius:50px;height:60px">Login</button>
+              <div class="row text-center text-secondary">
+                <div class="col-4 my-auto">
+                  <hr style="height:1.5px;border:none;color:#6C757D;background-color:#6C757D;">
+                </div>
+                <div class="col-4 my-auto">
+                  <p class="mb-0 ">Login With</p>
+                </div>
+                <div class="col-4 my-auto">
+                  <hr style="height:1.5px;border:none;color:#6C757D;background-color:#6C757D;">
+                </div>
+              </div>
+               <button class="btn btn-sub w-100 font-weight-bold my-3" style="border-radius:50px;height:60px"><b-icon-google></b-icon-google> Google</button>
+               <p class="text-center">Don’t have an account? <span class="text-main ">Sign Up</span></p>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-2 col-lg-3 my-auto">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
